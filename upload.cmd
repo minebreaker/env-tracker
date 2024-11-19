922b548a16cee908303771445381166dd7ead3cb
minebreaker <takutokoyama.terraflopsjapan@gmail.com>
2024-11-19 21:25:23 +0900

    fixed the duplicated line on upload.cmd

--- upload.cmd
+++ upload.cmd
@@ -5,7 +5,6 @@ ampy -p %PORT% put config.py
 ampy -p %PORT% put led.py
 ampy -p %PORT% put mhz19c.py
 ampy -p %PORT% put net.py
-ampy -p %PORT% put net.py
 ampy -p %PORT% put pins.py
 ampy -p %PORT% put tgs8100.py
 
